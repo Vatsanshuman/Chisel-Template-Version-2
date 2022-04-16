85f39560f05e9360315f5615d3d6c7404f817d8f
Vatsanshuman <anshuman23307@gmail.com>
2022-04-16 15:52:28 -0400

    make test done for verilator

---
+++ verilatorPrerequisites.sh
@@ -0,0 +1,13 @@
+sudo apt-get install git perl python3 make
+sudo apt-get install g++  # Alternatively, clang
+sudo apt-get install libgz  # Non-Ubuntu (ignore if gives error)
+sudo apt-get install libfl2  # Ubuntu only (ignore if gives error)
+sudo apt-get install libfl-dev  # Ubuntu only (ignore if gives error)
+sudo apt-get install zlibc zlib1g zlib1g-dev  # Ubuntu only (ignore if gives error)
+sudo apt-get install ccache  # If present at build, needed for run
+sudo apt-get install libgoogle-perftools-dev numactl perl-doc
+sudo apt-get install git autoconf flex bison
+sudo apt-get install gdb graphviz cmake clang clang-format-11 gprof lcov
+sudo pip3 install sphinx sphinx_rtd_theme breathe
+cpan install Pod::Perldoc
+cpan install Parallel::Forker
